1a47e9c2309bc726c126b3715bc9f7396bb08d3e
Ilya <emptydesert29@gmail.com>
2024-08-23 11:38:21 +0300

    feat: add h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/shared/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
+      <h1>Courses</h1>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
