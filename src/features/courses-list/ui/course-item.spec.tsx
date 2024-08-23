606758626a17913398bce51b4ca980a37e0c2ba9
Ilya <emptydesert29@gmail.com>
2024-08-23 11:31:10 +0300

    ci: add pull request check

---
+++ src/features/courses-list/ui/course-item.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { CourseItem } from "./courses-item";
+import userEvent from "@testing-library/user-event";
+
+describe("course item", () => {
+  it("should call delete callback", async () => {
+    const onDelete = jest.fn();
+    render(
+      <CourseItem
+        course={{
+          id: "dsfdsfs",
+          description: "dszfdsf",
+          name: "sdfdsf",
+        }}
+        onDelete={onDelete}
+      />,
+    );
+
+    await userEvent.click(screen.getByText("Удалить"));
+
+    expect(onDelete).toHaveBeenCalled();
+  });
+});
